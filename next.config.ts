4d8d35333d2b57ca8438fe375d6843200687c623
BiwPanarin <absolute41239@hotmail.com>
2024-12-28 23:05:21 +0700

    test_search_pokemon v.1.0.5 modify next.config.ts for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-import type { NextConfig } from 'next';
+import { NextConfig } from 'next';
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
+  reactStrictMode: true, // เปิด React Strict Mode
+  distDir: '.next', // ใช้ build output ค่าเริ่มต้น
+  swcMinify: true, // ใช้ SWC สำหรับการ minify
 };
 
 export default nextConfig;
